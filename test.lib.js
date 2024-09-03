ab2bbdd8b8bd793eb63e1f6032777d43cb4e663a
Vitor Mateus <vitor7jan@gmail.com>
2024-09-03 00:08:10 -0300

    improved test assert result

--- test.lib.js
+++ test.lib.js
@@ -2,9 +2,13 @@ export const assertEqual = (expected, test) => {
   const result = expected === test;
   result
     ? console.log("SUCCESS")
-    : console.error(`ERROR | expected=${expected} | result=${result}`);
+    : console.error(`ERROR | expected=${expected} | received=${test}`);
 };
 
 export const assertTrue = (result) => {
   result ? console.log("SUCCESS") : console.error("ERROR");
+};
+
+export const assertFalse = (result) => {
+  return assertTrue(!result);
 };
